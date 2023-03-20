56578720f222bbc68de19b9f9f98f19543fe9df0
JorgeLT1 <jorgeluistoribio14@gmail.com>
2023-03-20 18:56:51 -0400

    Create database

--- Program.cs
+++ Program.cs
@@ -1,13 +1,19 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using Parcial2_AP1_JorgeLuisToribio.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<ProductoBLL>();
 
 var app = builder.Build();
 
